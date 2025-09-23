e761ee82d3ef8b8e1bffa0ef024b10515db6525b
jefferson <jeffersonacostainoa@gmail.com>
2025-09-23 15:16:22 -0500

    operadores

---
+++ Datos.py
@@ -0,0 +1,6 @@
+numero = 20
+string = 'hola como estas'
+
+
+print(numero)
+print(type(numero))
---
+++ EntradaySalidaDatos.py
@@ -0,0 +1,6 @@
+nombre = input('escribe tu nombre: ')
+edad = int(input('escribe tu edad: '))
+
+print('hola',nombre)
+print('tendras', edad + 1, 'años el siguiente año')
+
